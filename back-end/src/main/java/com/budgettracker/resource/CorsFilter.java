379ca6e34ddff5a052892805e40ae5899234be5e
Dani Budo <56448641+danibudo@users.noreply.github.com>
2026-05-05 21:37:36 +0200

    handle options in cors

--- back-end/src/main/java/com/budgettracker/resource/CorsFilter.java
+++ back-end/src/main/java/com/budgettracker/resource/CorsFilter.java
@@ -1,12 +1,21 @@
 package com.budgettracker.resource;
 
 import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 
 @Provider
-public class CorsFilter implements ContainerResponseFilter {
+public class CorsFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
+            requestContext.abortWith(Response.ok().build());
+        }
+    }
 
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
