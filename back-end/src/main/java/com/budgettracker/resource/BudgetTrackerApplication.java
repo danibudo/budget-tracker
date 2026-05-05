d2a3b37fa5f912a57315585a3cef88f0bba0dc91
Dani Budo <56448641+danibudo@users.noreply.github.com>
2026-05-05 17:23:02 +0200

    add application class

---
+++ back-end/src/main/java/com/budgettracker/resource/BudgetTrackerApplication.java
@@ -0,0 +1,8 @@
+package com.budgettracker.resource;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class BudgetTrackerApplication extends Application {
+}
